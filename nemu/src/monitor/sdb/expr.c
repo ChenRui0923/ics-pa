a94281b2e401a3ac26b4ced2487359c47871bdda
ChenRui0923 <41950730+ChenRui0923@users.noreply.github.com>
2024-07-14 00:49:20 +0800

    >  compile NEMU
    Linux gpu04-NF5280M4 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     00:49:20 up 57 days, 15:00,  2 users,  load average: 0.23, 0.15, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -247,7 +247,7 @@ word_t eval(word_t p, word_t q) {
     printf("Main operator: %d (%c) at %d\n", tokens[op].type, tokens[op].type, op);
     word_t val1 = eval(p, op - 1);
     word_t val2 = eval(op + 1, q);
-
+    printf("val1: %u, val2: %u, op: %c\n", val1, val2, tokens[op].type); // 添加打印
     switch (tokens[op].type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
