01510d47cd7513df0346c04294a791e27e4f1bc5
ChenRui0923 <41950730+ChenRui0923@users.noreply.github.com>
2024-07-13 14:53:46 +0800

    >  compile NEMU
    Linux gpu04-NF5280M4 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     14:53:46 up 57 days,  5:04,  1 user,  load average: 0.02, 0.03, 0.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -22,7 +22,7 @@ static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
   0x00028823,  // sb  zero,16(t0)
   0x0102c503,  // lbu a0,16(t0)
-  0x00100073,  // ebreak (used as nemu_trap)
+  // 0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
 
@@ -37,7 +37,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-    
+
   /* Initialize this virtual computer system. */
   restart();
 }
